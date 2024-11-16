7699caa0a29893cf69d14b4e26396e579b355879
Anirudh Singh <violentanirudh@gmail.com>
2024-11-16 19:55:51 +0530

    Scroll Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ const technology = ['Tailwind', 'JavaScript', 'React', 'NodeJS', 'MongoDB', 'Pyt
 function App() {
   return (
       <div className="bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-950 h-screen w-screen relative text-slate-200">
-          <div className="absolute inset-0 bg-slate-950/60 overflow-y-scroll">
+          <div className="absolute inset-0 bg-slate-950/60">
               <div className="bg-[url('/graph.svg')] bg-repeat min-h-screen relative">
                   <div className="h-screen w-screen overflow-y-scroll">
                       <div className="mx-auto max-w-screen-md px-4 py-10 sm:px-2 space-y-20">
